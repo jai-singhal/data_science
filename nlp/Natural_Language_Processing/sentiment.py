bbc8f718b11be4e66c8fbce417015695e7ad60ae
Jai Singhal <jaisinghal48@gmail.com>
2019-03-17 14:37:33 +0530

    add sentiment notebooks

--- nlp/Natural_Language_Processing/sentiment.py
+++ nlp/Natural_Language_Processing/sentiment.py
@@ -19,30 +19,28 @@ from nltk.tokenize import word_tokenize
 # In[3]:
 
 
-class VoteClassifier(ClassifierI):
-    def __init__(self, *classifiers):
-        self._classifiers = classifiers
+# class VoteClassifier(ClassifierI):
+#     def __init__(self, *classifiers):
+#         self._classifiers = classifiers
 
-    def classify(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
-        return mode(votes)
+#     def classify(self, features):
+#         votes = []
+#         for c in self._classifiers:
+#             v = c.classify(features)
+#             votes.append(v)
+#         return mode(votes)
 
-    def confidence(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
+#     def confidence(self, features):
+#         votes = []
+#         for c in self._classifiers:
+#             v = c.classify(features)
+#             votes.append(v)
 
-        choice_votes = votes.count(mode(votes))
-        conf = choice_votes / len(votes)
-        return conf
+#         choice_votes = votes.count(mode(votes))
+#         conf = choice_votes / len(votes)
+#         return conf
  
 
-
-# In[10]:
 
 
 short_pos = open("reviews/positive.txt","r").read()
--- regression/multiple_linear_regression/multiple_linear_regression.py
+++ regression/multiple_linear_regression/multiple_linear_regression.py
@@ -1,32 +1,103 @@
-# Multiple Linear Regression
-
-# Importing the libraries
-import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 4].values
-
-# Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder = LabelEncoder()
-X[:, 3] = labelencoder.fit_transform(X[:, 3])
-onehotencoder = OneHotEncoder(categorical_features = [3])
-X = onehotencoder.fit_transform(X).toarray()
-
-# Avoiding the Dummy Variable Trap
-X = X[:, 1:]
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+import math
+import googlemaps 
+import xlrd
+import csv
 
 
-# Fitting Multiple Linear Regression to the Training set
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+class CalculateDistance:
+	def __init__(self, apiKey):
+		self.API_KEY = apiKey
 
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
+	# # Not in use
+	# def haversineDistance(self, src, destination):
+	# 	# https://en.wikipedia.org/wiki/Haversine_formula
+	# 	lat1, lon1 = src
+	# 	lat2, lon2 = destination
+	# 	radius = 6371
+	# 	dlat = math.radians(lat2 - lat1)
+	# 	dlon = math.radians(lon2 - lon1)
+	# 	result = (math.sin(dlat / 2) * math.sin(dlat / 2) +
+	# 	     math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
+	# 	     math.sin(dlon / 2) * math.sin(dlon / 2))
+	# 	result = 2 * math.atan2(math.sqrt(result), math.sqrt(1 - result))
+	# 	distance = radius * result
+	# 	return distance
+
+
+	def googleMapDistance(self, src, destination):
+		gmaps = googlemaps.Client(key=self.API_KEY, timeout=3, retry_timeout=5)
+		try:
+			result = gmaps.distance_matrix(
+						[str(src[0]) + " " + str(src[1])], 
+						[str(destination[0]) + " " + str(destination[1])])
+		except Exception as e:
+			print("Something went wrong with making API Call")
+			exit(1)
+
+		try:
+			distance = result['rows'][0]['elements'][0]["distance"] # in km
+			#distance = "{} miles".format(0.621371*distance) 
+			duration = result['rows'][0]['elements'][0]["duration"]
+			return(distance, duration) 
+		except:
+			print("Google map api fails: {}, {} not reachable".format(src, destination))
+			return False
+
+
+	def read(self, path):
+		workbook = xlrd.open_workbook(path)
+		worksheet = workbook.sheet_by_index(0)
+		first_row = []
+		for col in range(worksheet.ncols):
+		    first_row.append(worksheet.cell_value(0,col))
+		data = []
+		for row in range(1, worksheet.nrows):
+		    elment = []
+		    for col in range(worksheet.ncols):
+		        elment.append(worksheet.cell_value(row, col))
+		    data.append(elment)
+		return data
+
+
+	def write(self, data):
+		keys = data[0].keys()
+		with open('output.csv', 'w', newline='') as output_file:
+		    dict_writer = csv.DictWriter(output_file, keys)
+		    dict_writer.writeheader()
+		    dict_writer.writerows(data)
+
+
+	def calculateGMapDistance(self, path):
+		input_data = self.read(path)
+		distances = []
+		for d in input_data:
+			src = d[1], d[2]  #latitide longitude
+			dest = d[3], d[4] #latitide longitude
+			response = self.googleMapDistance(src, dest)
+			if response: # if successful api response
+				distance, duration = response
+				distances.append({
+					"lane": d[0],
+					"distance": distance["text"],
+					"duration": duration["text"]
+				})
+			else: # if failure api response
+				distances.append({
+					"lane": d[0],
+					"distance": "",
+					"duration": ""
+				})
+		self.write(distances)
+		print("Successfully calculated Google Map Distances")
+
+
+def main():
+	apiKey = "AIzaSyBWxiVyKaCvJWLziWYuXrdY8s4LRUqFMIw"
+	inputFileLoc = "input.xlsx"
+
+	d = CalculateDistance(apiKey)
+	d.calculateGMapDistance(inputFileLoc)
+
+
+if __name__ == '__main__':
+	main()
